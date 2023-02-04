7e034f57912c99b29601ad604f9b5068e947f721
Divyesh P K <50244658+pkdiv@users.noreply.github.com>
2023-02-04 13:37:41 +0530

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,25 +1,42 @@
 import ast
-strInput = "demoString"
-strInput = strInput.upper()
+import argparse
 
+def argumentsParse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-w", "--word", help="word to generate the leetspeak for")
+    parser.add_argument("-o", "--outputFile", help="Output filename")
+    parser.add_argument("-l", "--level", help="Specify the aggressiveness of the list", type=int)
+    args = parser.parse_args()
+    return args
 
-stringLength = len(strInput)
+def varInits():
+    args = argumentsParse()
+    strInput = args.word.upper()
+    stringLength = len(strInput)
+    outputFile = args.outputFile if args.outputFile else 'wordlist'
+    level = args.level
+    return [strInput, stringLength, outputFile, level]
 
-with open("leetspeakListSmall", 'r', encoding="utf8") as fl:
-    data = fl.read()
-dct = ast.literal_eval(data)
-
-stackList = []
+def listSelect(level):
+    fileList = ['leetspeakListSmall', 'leetspeakList']
+    with open('Lists/' + fileList[level], 'r', encoding="utf8") as fl:
+        data = fl.read()
+    dct = ast.literal_eval(data)
+    return dct
 
 def leetSpeakGen(index=0):
     alphaList = dct[strInput[index]]
     for character in alphaList:
         stackList.append(character)
         if(index == stringLength - 1):
-            print(''.join(map(str, stackList)))
+            file.write(''.join(map(str, stackList)) + '\n')
         else:
             leetSpeakGen(index+1)
         stackList.pop()
 
-
-leetSpeakGen()
+strInput, stringLength, outputFile, level = varInits()
+dct = listSelect(level=0)
+stackList = []
+with open(outputFile, 'w+', encoding="utf8") as file:
+    leetSpeakGen()  
+print(f'Wordlist generated and stored in {outputFile}')
